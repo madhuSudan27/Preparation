ea76c0dd6b7671c469a2ace59f1237e9eeebdd0c
madhuSudan27 <122591446+madhuSudan27@users.noreply.github.com>
2023-10-11 00:25:10 +0530

    Advancd Concepts

---
+++ DSA/BinarySearch/Advance/SquareRoot.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int root(int n) {
+    int low = 1, high = n;
+    //Binary search on the answers:
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long val = mid * mid;
+        if (val <= (long long)(n)) {
+            //eliminate the left half:
+            low = mid + 1;
+        }
+        else {
+            //eliminate the right half:
+            high = mid - 1;
+        }
+    }
+    return high;
+}
