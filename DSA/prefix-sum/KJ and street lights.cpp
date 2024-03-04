5241f73690300ade343e7aed7b03e5e07e0405cf
madhuSudan27 <122591446+madhuSudan27@users.noreply.github.com>
2024-03-04 21:55:54 +0530

    scanning algorithm

---
+++ DSA/prefix-sum/KJ and street lights.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// scanning algorithm 
+
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
+     int n , p;
+    cin >> n >> p;
+    vector<int>helper(p+2 , 0);
+    
+    for(int index = 0; index < n; index ++){
+        int xi, ri;
+        cin >> xi >> ri;
+        int start = max(0, xi - ri);
+        int end = min( xi + ri , p);
+        helper[start] ++ ;
+        helper[end + 1] --;
+    }
+    
+    for(int index = 1; index <= p; index ++){
+        helper[index] = helper[index - 1] + helper[index];
+    }
+
+    int maxDark = 0;
+    int currDark = 0;
+    for(int index = 0; index <= p; index ++){
+        if(helper[index] == 1){
+            maxDark = max(maxDark, currDark);
+            currDark = 0;
+        }
+        else{
+            currDark ++;
+        }
+        
+    }
+    maxDark = max(maxDark , currDark);
+    
+    cout<< maxDark << endl;
+    return 0;
+}
